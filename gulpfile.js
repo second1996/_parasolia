b5f6600c4b94d85979c11320059d346591a08612
second1996 <vital10012@gmail.com>
2022-01-17 12:43:19 +0200

    git init

---
+++ gulpfile.js
@@ -0,0 +1,159 @@
+let preprocessor = 'sass', // Preprocessor SASS also work with the Scss syntax in blocks/ folder.
+  fileswatch = 'html,htm,txt,json,md,woff2'; // List of files extensions for watching & hard reload
+
+const { src, dest, parallel, series, watch } = require('gulp');
+const browserSync = require('browser-sync').create();
+const bssi = require('browsersync-ssi');
+const ssi = require('ssi');
+const webpack = require('webpack-stream');
+const sass = require('gulp-sass')(require('sass'));
+const sassglob = require('gulp-sass-glob');
+const cleancss = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
+const mediaqueries = require('gulp-group-css-media-queries');
+const rename = require('gulp-rename');
+const imagemin = require('gulp-imagemin');
+const newer = require('gulp-newer');
+const svgsprite = require('gulp-svg-sprite');
+const cheerio = require('gulp-cheerio');
+const rsync = require('gulp-rsync');
+const del = require('del');
+
+function browsersync() {
+  browserSync.init({
+    server: {
+      baseDir: 'app/',
+      middleware: bssi({ baseDir: 'app/', ext: '.html' }),
+    },
+    ghostMode: { clicks: false },
+    notify: false,
+    online: true,
+    // tunnel: 'yousutename', // Attempt to use the URL https://yousutename.loca.lt
+  });
+}
+
+function scripts() {
+  return src(['app/js/*.js', '!app/js/*.min.js'])
+    .pipe(
+      webpack({
+        mode: 'production',
+        performance: { hints: false },
+        module: {
+          rules: [
+            {
+              test: /\.(js)$/,
+              exclude: /(node_modules)/,
+              loader: 'babel-loader',
+              query: {
+                presets: ['@babel/env'],
+                plugins: ['babel-plugin-root-import'],
+              },
+            },
+          ],
+        },
+      }),
+    )
+    .on('error', function handleError() {
+      this.emit('end');
+    })
+    .pipe(rename('app.min.js'))
+    .pipe(dest('app/js'))
+    .pipe(browserSync.stream());
+}
+
+function styles() {
+  return src([`app/${preprocessor}/*.*`, `!app/${preprocessor}/_*.*`])
+    .pipe(eval(`${preprocessor}glob`)())
+    .pipe(eval(preprocessor)())
+    .pipe(mediaqueries())
+    .pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true }))
+    .pipe(cleancss({ level: { 1: { specialComments: 0 } }, format: 'beautify' }))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(dest('app/css'))
+    .pipe(browserSync.stream());
+}
+
+function images() {
+  return src(['app/images/src/**/*'])
+    .pipe(newer('app/images/dist'))
+    .pipe(imagemin())
+    .pipe(dest('app/images/dist'))
+    .pipe(browserSync.stream());
+}
+
+function svgSprite() {
+  return src(['app/svg-icons/*.svg'])
+    .pipe(
+      svgsprite({
+        mode: {
+          symbol: {
+            sprite: '../symbol-defs.svg',
+          },
+        },
+      }),
+    )
+    .pipe(
+      cheerio({
+        run: function ($) {
+          $('[fill]').removeAttr('fill');
+          $('[stroke]').removeAttr('stroke');
+          $('[style]').removeAttr('style');
+          $('style').remove();
+        },
+        parserOptions: { xmlMode: true },
+      }),
+    )
+    .pipe(dest('app/images'));
+}
+
+function buildcopy() {
+  return src(
+    ['{app/js,app/css}/*.min.*', 'app/images/**/*.*', '!app/images/src/**/*', 'app/fonts/**/*'],
+    { base: 'app/' },
+  ).pipe(dest('dist'));
+}
+
+async function buildhtml() {
+  let includes = new ssi('app/', 'dist/', '/**/*.html');
+  includes.compile();
+  del('dist/parts', { force: true });
+}
+
+function cleandist() {
+  return del('dist/**/*', { force: true });
+}
+
+function deploy() {
+  return src('dist/').pipe(
+    rsync({
+      root: 'dist/',
+      hostname: 'username@yousite.com',
+      destination: 'yousite/public_html/',
+      // clean: true, // Mirror copy with file deletion
+      include: [
+        /* '*.htaccess' */
+      ], // Included files to deploy,
+      exclude: ['**/Thumbs.db', '**/*.DS_Store'],
+      recursive: true,
+      archive: true,
+      silent: false,
+      compress: true,
+    }),
+  );
+}
+
+function startwatch() {
+  watch(`app/${preprocessor}/**/*`, { usePolling: true }, series(styles));
+  watch(['app/js/**/*.js', '!app/js/**/*.min.js'], { usePolling: true }, scripts);
+  watch('app/images/src/**/*.{jpg,jpeg,png,webp,svg,gif}', { usePolling: true }, images);
+  watch('app/svg-icons/*.svg', { usePolling: true }, svgSprite);
+  watch(`app/**/*.{${fileswatch}}`, { usePolling: true }).on('change', browserSync.reload);
+}
+
+exports.scripts = scripts;
+exports.styles = styles;
+exports.images = images;
+exports.deploy = deploy;
+exports.assets = series(scripts, styles, images, svgSprite);
+exports.build = series(cleandist, scripts, styles, images, svgSprite, buildcopy, buildhtml);
+exports.default = series(scripts, styles, images, svgSprite, parallel(browsersync, startwatch));
