eabfedea792329e2a1e8afeee3d076def297ecf9
second1996 <vital10012@gmail.com>
2022-01-19 12:49:59 +0200

    Heroes slider animation

--- app/js/_swiper.js
+++ app/js/_swiper.js
@@ -3,8 +3,9 @@ import Swiper, { Pagination, Navigation, EffectFade, Autoplay } from 'swiper';
 Swiper.use([Pagination, EffectFade, Autoplay]);
 
 const heroesSwiper = new Swiper('.h-heroes-slider', {
+  speed: 1000,
   autoplay: {
-    delay: 3000,
+    delay: 4000,
     pauseOnMouseEnter: false,
     disableOnInteraction: false,
   },
@@ -17,18 +18,17 @@ const heroesSwiper = new Swiper('.h-heroes-slider', {
     type: 'bullets',
     clickable: true,
   },
+  allowTouchMove: false,
 });
 
 // Slider navigation
 const sliderNavItem = document.querySelectorAll('.h-heroes-slider-nav-item');
+const sliderDelay = heroesSwiper.params.autoplay.delay + heroesSwiper.params.speed;
 
 sliderNavItem[0].classList.add('_is-active');
 
 sliderNavItem.forEach((el) => {
-  el.querySelector('.progress-line').setAttribute(
-    'style',
-    `animation-duration: ${heroesSwiper.params.autoplay.delay + 250}ms`,
-  );
+  el.querySelector('.progress-line').setAttribute('style', `animation-duration: ${sliderDelay}ms`);
   el.addEventListener('click', (event) => {
     let slideIndex = event.currentTarget.dataset.slideIndex;
     el.classList.add('_is-active');
@@ -44,5 +44,29 @@ heroesSwiper.on('slideChange', (swiper) => {
     if (index === swiper.activeIndex) {
       el.classList.add('_is-active');
     }
+  });
+});
+
+// Slider symbols
+const heroesSymbol = document.querySelector('.h-heroes-symbol');
+const sliderSymbols = document.querySelectorAll('.h-heroes-slider-symbols .symbol');
+
+heroesSwiper.on('slideChangeTransitionStart', () => {
+  heroesSymbol.classList.remove('_is-shown');
+  heroesSymbol.classList.add('_is-hidden');
+
+  sliderSymbols.forEach((el) => {
+    el.classList.remove('_is-shown');
+    el.classList.add('_is-hidden');
+  });
+});
+
+heroesSwiper.on('slideChangeTransitionEnd', () => {
+  heroesSymbol.classList.remove('_is-hidden');
+  heroesSymbol.classList.add('_is-shown');
+
+  sliderSymbols.forEach((el) => {
+    el.classList.remove('_is-hidden');
+    el.classList.add('_is-shown');
   });
 });
