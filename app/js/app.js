5b918cc9a83a60a315254f6317619c6f3626c769
second1996 <vital10012@gmail.com>
2022-01-21 16:03:56 +0200

    Section: Callback. Footer. Mobile responsive. Added imaskjs library

--- app/js/app.js
+++ app/js/app.js
@@ -3,10 +3,21 @@
 // window.jQuery = $
 // window.$ = $
 
+import IMask from 'imask'; // Import Input mask
+
 // // Import vendor jQuery plugin example (not module)
 // require('~/app/libs/mmenu/dist/mmenu.js')
 
 document.addEventListener('DOMContentLoaded', () => {
-  // Import Swiper
-  require('./_swiper.js');
+  require('./_swiper.js'); // Import Swiper
+
+  // Apply phone mask for inputs
+  const phoneInputs = document.querySelectorAll('input[type="phone"]');
+  const maskOptions = {
+    mask: '+{38} (000) 000-00-00',
+  };
+
+  phoneInputs.forEach((el) => {
+    let mask = IMask(el, maskOptions);
+  });
 });
