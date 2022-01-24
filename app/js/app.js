986ff7f6174431f74c9c242b1d6b77afe0dafd5f
second1996 <vital10012@gmail.com>
2022-01-24 12:57:03 +0200

    Mobile menu. Sticky header

--- app/js/app.js
+++ app/js/app.js
@@ -20,4 +20,39 @@ document.addEventListener('DOMContentLoaded', () => {
   phoneInputs.forEach((el) => {
     let mask = IMask(el, maskOptions);
   });
+
+  // Mobile menu
+  const headerEl = document.querySelector('.header');
+  const burgerBtn = document.querySelector('.header-menu-burger .btn-burger');
+  const mobileMenu = document.querySelector('.m-menu');
+
+  burgerBtn.addEventListener('click', (e) => {
+    e.currentTarget.classList.toggle('_is-opened');
+    headerEl.classList.toggle('_is-active');
+    mobileMenu.classList.toggle('_is-shown');
+  });
+
+  // Sticky header
+  const heroesSection = document.querySelector('.h-heroes');
+  const headerOffset = heroesSection.clientHeight || headerEl.offsetTop;
+
+  window.addEventListener('scroll', function () {
+    stickyHeader();
+
+    if (this.oldScroll < this.scrollY) {
+      headerEl.classList.add('_is-hidden');
+    } else {
+      headerEl.classList.remove('_is-hidden');
+    }
+
+    this.oldScroll = this.scrollY;
+  });
+
+  const stickyHeader = () => {
+    if (window.pageYOffset > headerOffset) {
+      headerEl.classList.add('_is-sticky');
+    } else {
+      headerEl.classList.remove('_is-sticky');
+    }
+  };
 });
