84b66e55a28fa443ea501b56fd2a12baea27d7ec
second1996 <vital10012@gmail.com>
2022-01-31 20:50:06 +0200

    Calculator

--- app/js/_calculator.js
+++ app/js/_calculator.js
@@ -1,26 +1,7 @@
-const calcForm = document.querySelector('#calc-form');
-const calcOrganizationInputs = document.querySelectorAll('input[name*="calc-organization"]');
-const calcIndividual = document.querySelector('#individual');
-const calcLegal = document.querySelector('#legal');
-
-calcOrganizationInputs.forEach((el) => {
-  el.addEventListener('change', (e) => {
-    let organization = e.currentTarget.dataset.organization;
-
-    if (organization === 'individual') {
-      calcIndividual.classList.remove('d-none');
-      calcLegal.classList.add('d-none');
-    }
-
-    if (organization === 'legal') {
-      calcLegal.classList.remove('d-none');
-      calcIndividual.classList.add('d-none');
-    }
-  });
-});
-
-const quizIndividual = [
+// Individual data
+const individualData = [
   {
+    id: 0,
     question: 'На якій ви системі оподаткування?',
     descr: 'Оберіть систему оподаткування вашого ФОП чи ТОВ.',
     answers: [
@@ -28,21 +9,23 @@ const quizIndividual = [
         text: 'Спрощена система',
         nextNode: 'tax-group',
       },
+
       {
         text: 'Загальна система',
         nextNode: 'tax-payer',
       },
     ],
     node: 'tax-system',
-    parentNode: null,
   },
   {
+    id: 1,
     question: 'Яка у вас група єдиного податку?',
     descr: 'Оберіть систему оподаткування вашого ФОП чи ТОВ.',
     answers: [
       {
         text: '2 група',
         nextNode: 'workers',
+        value: 300,
       },
       {
         text: '3 група',
@@ -50,9 +33,9 @@ const quizIndividual = [
       },
     ],
     node: 'tax-group',
-    parentNode: 'tax-system',
   },
   {
+    id: 2,
     question: 'Чи є ви платником ПДВ',
     descr: 'Оберіть систему оподаткування вашого ФОП чи ТОВ.',
     answers: [
@@ -66,30 +49,118 @@ const quizIndividual = [
       },
     ],
     node: 'tax-payer',
-    parentNode: 'tax-system',
   },
   {
+    id: 3,
     question: 'Чи є у вас наймані працівники?',
     descr: 'Оберіть систему оподаткування вашого ФОП чи ТОВ.',
     answers: [
       {
         text: 'Ні',
         nextNode: null,
+        value: 1500,
       },
       {
         text: 'Так',
         nextNode: null,
+        value: 1500,
+        multiplier: 700,
+        additional: {
+          question: 'Кількість працівників?',
+          descr: 'Введіть числове значення к-ті працівників у вашій компанії.',
+          field: 'Кількість працівників',
+        },
       },
     ],
     node: 'workers',
-    parentNode: 'tax-group',
   },
 ];
 
-function renderCard(obj) {
-  const { question, descr, answers, node } = obj;
+let totalSum = 0;
 
-  return `<div class="calculator-section" data-section-id="${node}">
+const calcForm = document.querySelector('#calc-form');
+const calcOrganizationInputs = document.querySelectorAll('input[name*="calc-organization"]');
+const calcIndividual = document.querySelector('#individual');
+const calcLegal = document.querySelector('#legal');
+
+// Tabs
+calcOrganizationInputs.forEach((el) => {
+  el.addEventListener('change', (e) => {
+    let organization = e.currentTarget.dataset.organization;
+
+    totalSum = 0;
+
+    if (organization === 'individual') {
+      calcIndividual.classList.remove('d-none');
+      calcLegal.classList.add('d-none');
+
+      calcLegal.innerHTML = '';
+      calcIndividual.insertAdjacentHTML('afterbegin', renderCard(individualData[0]));
+    }
+
+    if (organization === 'legal') {
+      calcIndividual.innerHTML = '';
+
+      calcLegal.classList.remove('d-none');
+      calcIndividual.classList.add('d-none');
+    }
+  });
+});
+
+// Individual logic
+calcIndividual.addEventListener('click', (e) => {
+  totalSum = 0;
+
+  if (e.target.className === 'options-check-input') {
+    e.target.onchange = (el) => {
+      let nextNodeId= el.target.dataset.nextNodeId;
+      let currentIndex = el.target.closest('.calculator-section').dataset.sectionIndex;
+      let cardObj = individualData.find((obj) => obj.node === nextNodeId);
+      let sections = el.target.closest('.calculator-tree').querySelectorAll('.calculator-section');
+      let inputs = el.target.closest('.calculator-tree').querySelectorAll('.options-check-input');
+
+      // Render card
+      if (nextNodeId !== 'null') {
+        calcIndividual.insertAdjacentHTML('beforeend', renderCard(cardObj));
+      }
+
+      // Removes sections that section index bigger than current section index
+      for (const section of sections) {
+        if (section.dataset.sectionIndex > currentIndex) {
+          section.querySelectorAll('.options-check-input').forEach((el) => {
+            if (el.checked) {
+              totalSum -= +el.dataset.value;
+
+              if (el.dataset.multiplier) {
+                totalSum -= +el.dataset.multiplier;
+              }
+            }
+          });
+
+          section.remove();
+        }
+      }
+
+      // Calculate total sum
+      inputs.forEach((el) => {
+        if (el.checked && el.dataset.value) {
+          totalSum += +el.dataset.value;
+
+          if (el.dataset.multiplier) {
+            totalSum += +el.dataset.multiplier;
+          }
+        }
+      });
+
+      console.log(totalSum);
+    };
+  }
+});
+
+function renderCard(obj) {
+  const { id, question, descr, answers, node } = obj;
+
+  return `<div class="calculator-section" data-section-id="${node}" data-section-index="${id}">
       <div class="heading">
         <h3 class="heading-title text-lg">${question}</h3>
         <div class="heading-descr">
@@ -98,11 +169,23 @@ function renderCard(obj) {
       </div>
       <div class="options">
       ${answers
-        .map(({ text, nextNode }) => {
+        .map(answer => {
+          if (answer.additional) {
+            const { question, descr, field } = answer.additional;
+
+            console.log(field);
+          }
+
           return `<label class="options-check">
-            <input class="options-check-input" type="radio" name="calc-individual-${node}[]" data-next-node-id="${nextNode}">
+            <input 
+              class="options-check-input" 
+              type="radio" 
+              name="calc-individual-${node}[]" 
+              data-next-node-id="${answer.nextNode}" 
+              ${answer.value ? `data-value="${answer.value}"` : ''} 
+              ${answer.multiplier ? `data-multiplier="${answer.multiplier}"` : ''}>
             <span class="options-check-box"></span>
-            ${text}
+            ${answer.text}
           </label>`;
         })
         .join('')}
@@ -110,46 +193,6 @@ function renderCard(obj) {
     </div>`;
 }
 
-// Render first card
-quizIndividual.forEach((obj, index) => {
-  if (index === 0) {
-    calcIndividual.innerHTML = renderCard(obj);
-  }
-});
-
-// Individual logic
-let calcCheckInputs = document.querySelectorAll('.options-check-input');
-
-calcIndividual.addEventListener('click', (e) => {
-  if (e.target.className === 'options-check-input') {
-    let sectionId = e.target.closest('.calculator-section').dataset.sectionId;
-
-    e.target.addEventListener('change', (e) => {
-      let nextNodeId = e.currentTarget.dataset.nextNodeId;
-      console.log(sectionId);
-
-      if (e.target.closest('.calculator-section').dataset.sectionId !== sectionId) {
-        const result = quizIndividual.find((obj) => {
-          return obj.node == nextNodeId;
-        });
-
-        calcIndividual.innerHTML += renderCard(result);
-      }
-    });
-  }
-});
-
-// let previousSectionId;
-// calcCheckInputs.forEach((el) => {
-//   el.addEventListener('change', (e) => {
-//     let sectionId = e.currentTarget.dataset.nextNodeId;
-//     // let sectionEl = el
-//     //   .closest('.calculator-tree')
-//     //   .querySelector(`.calculator-section[data-section-id=${sectionId}]`);
-
-//     previousSectionId = sectionId;
-
-//     calcIndividual.querySelector(`[data-section-id="${previousSectionId}"`).classList.add('d-none');
-//     calcIndividual.querySelector(`[data-section-id="${sectionId}"`).classList.remove('d-none');
-//   });
-// });
+function appendSection(cardObj) {
+  calcIndividual.insertAdjacentHTML('beforeend', renderCard(cardObj));
+}
